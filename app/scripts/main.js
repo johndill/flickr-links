db0e39a68343916f35a37163d238f772c485bc50
John Dill <dill.john.d@gmail.com>
2015-05-31 16:56:44 -0400

    added routing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,31 @@
 
 'use strict';
 
-var myApp = angular.module('flApp', ['firebase']);
+var myApp = angular.module('flApp', ['firebase', 'ngRoute']);
 
 myApp.controller('galleryCtrl', ['$scope', '$firebaseArray',
-	function($scope, $firehaseArray) {
-		var ref = new Firebase('https://flickr-links.firebaseio.com/');
-		$scope.albums = $firehaseArray(ref.child('albums'));
-	}
+  function($scope, $firehaseArray) {
+    var ref = new Firebase('https://flickr-links.firebaseio.com/');
+    $scope.albums = $firehaseArray(ref.child('albums'));
+  }
+]);
+
+myApp.controller('loginCtrl', ['$scope',
+  function($scope) {
+    $scope.user = 'none';
+  }
+]);
+
+myApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'loginCtrl',
+    })
+    .otherwise({
+      templateUrl: 'partials/albums.html',
+      controller: 'galleryCtrl'
+    });
+  }
 ]);
