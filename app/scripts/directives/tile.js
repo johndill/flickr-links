6cf133b5f684b96f32be82dd423a17e66c7f2e09
John Dill <dill.john.d@gmail.com>
2015-06-01 16:06:55 -0400

    implemented tile as a directive

---
+++ app/scripts/directives/tile.js
@@ -0,0 +1,16 @@
+/*global myApp*/
+
+'use strict';
+
+myApp.directive('tile', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			url: '@url',
+			image: '@image',
+			title: '@title',
+			description: '@description'
+		},
+		templateUrl: 'scripts/directives/tile.html'
+	};
+});
